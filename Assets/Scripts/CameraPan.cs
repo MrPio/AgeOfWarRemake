8b2432496a98e57a4f5b4dbff9a8f22d9e455658
Valerio Morelli <valeriomorelli50@gmail.com>
2025-06-06 23:14:16 +0200

    Add camera movement

---
+++ Assets/Scripts/CameraPan.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class CameraEdgePan : MonoBehaviour
+{
+    [Header("Movement Settings")] [SerializeField]
+    private float edgeThreshold = 200f, moveSpeed = 5f;
+
+    [SerializeField] private Vector2 xBounds = new(-10f, 10f);
+    [SerializeField] private Camera cam;
+    private float _currentSpeed;
+
+    private void Update()
+    {
+        var speed = 0f;
+        var mouseX = Input.mousePosition.x;
+        var camPos = cam.transform.position;
+
+        if (mouseX < edgeThreshold)
+        {
+            var t = Mathf.Clamp(1 - mouseX / edgeThreshold, 0, 1);
+            speed = -moveSpeed * Mathf.Pow(t, 3);
+        }
+        else if (mouseX > Screen.width - edgeThreshold)
+        {
+            var t = Mathf.Clamp(1 - (Screen.width - mouseX) / edgeThreshold, 0, 1);
+            speed = moveSpeed * Mathf.Pow(t, 3);
+        }
+
+        var x = Mathf.Clamp(camPos.x + speed * Time.deltaTime, xBounds.x, xBounds.y);
+        cam.transform.position = new Vector3(x, camPos.y, camPos.z);
+    }
+}
