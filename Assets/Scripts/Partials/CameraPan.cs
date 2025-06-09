f23ffe3c23a7dee1dd56094734da776303327a4d
Valerio Morelli <valeriomorelli50@gmail.com>
2025-06-10 01:09:04 +0200

    Add camera drag behavior

--- Assets/Scripts/Partials/CameraPan.cs
+++ Assets/Scripts/Partials/CameraPan.cs
@@ -1,4 +1,3 @@
-using System;
 using Managers;
 using UnityEngine;
 
@@ -6,17 +5,19 @@ namespace Partials
 {
     public class CameraEdgePan : MonoBehaviour
     {
-        [Header("Movement Settings")] [SerializeField]
-        private float edgeThreshold = 200f, moveSpeed = 5f;
-
-        [SerializeField] private readonly float marginFromBase = 3.4f;
+        [SerializeField] private float edgeThreshold = 225f, edgeScrollSensitivity = 5f;
+        [SerializeField] private float marginFromBase = 2.8f;
+        [SerializeField] private float dragSensitivity = 0.01f;
         [SerializeField] private Camera cam;
         private float _currentSpeed;
         private Vector2 _boundX;
         private static SceneManager _sm;
+        private bool isDragging;
+        private float lastMouseX;
+
         private void Awake()
         {
-            _sm=GameObject.FindWithTag("SceneManager").GetComponent<SceneManager>();
+            _sm = GameObject.FindWithTag("SceneManager").GetComponent<SceneManager>();
         }
 
         private void Start()
@@ -28,28 +29,49 @@ namespace Partials
 
         private void Update()
         {
+            if (Input.GetMouseButtonDown(0))
+            {
+                isDragging = true;
+                lastMouseX = Input.mousePosition.x;
+            }
+            else if (Input.GetMouseButtonUp(0))
+                isDragging = false;
+
             var actualXBound =
-                _boundX / (((float)Screen.width / Screen.height) / (16f / 9f)); // Window may resize runtime
-            var speed = 0f;
-            var mouseX = Input.mousePosition.x;
-            var mouseY = Input.mousePosition.y;
-            if (mouseX > Screen.width || mouseX < 0) return;
-            if (mouseY > Screen.height || mouseY < 0) return;
-
+                _boundX / ((float)Screen.width / Screen.height / (16f / 9f)); // Window may resize runtime
             var camPos = cam.transform.position;
+            float deltaX;
 
-            if (mouseX < edgeThreshold)
+            // Drag behaviour
+            if (isDragging)
             {
-                var t = Mathf.Clamp(1 - mouseX / edgeThreshold, 0, 1);
-                speed = -moveSpeed * Mathf.Pow(t, 3);
+                deltaX = (lastMouseX - Input.mousePosition.x) * dragSensitivity;
+                lastMouseX = Input.mousePosition.x;
             }
-            else if (mouseX > Screen.width - edgeThreshold)
+            // Edge scroll behaviour
+            else
             {
-                var t = Mathf.Clamp(1 - (Screen.width - mouseX) / edgeThreshold, 0, 1);
-                speed = moveSpeed * Mathf.Pow(t, 3);
+                var speed = 0f;
+                var mouseX = Input.mousePosition.x;
+                var mouseY = Input.mousePosition.y;
+                if (mouseX > Screen.width || mouseX < 0) return;
+                if (mouseY > Screen.height || mouseY < 0) return;
+
+                if (mouseX < edgeThreshold)
+                {
+                    var t = Mathf.Clamp(1 - mouseX / edgeThreshold, 0, 1);
+                    speed = -edgeScrollSensitivity * Mathf.Pow(t, 3);
+                }
+                else if (mouseX > Screen.width - edgeThreshold)
+                {
+                    var t = Mathf.Clamp(1 - (Screen.width - mouseX) / edgeThreshold, 0, 1);
+                    speed = edgeScrollSensitivity * Mathf.Pow(t, 3);
+                }
+
+                deltaX = speed * Time.deltaTime;
             }
 
-            var x = Mathf.Clamp(camPos.x + speed * Time.deltaTime, actualXBound.x, actualXBound.y);
+            var x = Mathf.Clamp(camPos.x + deltaX, actualXBound.x, actualXBound.y);
             cam.transform.position = new Vector3(x, camPos.y, camPos.z);
         }
     }
